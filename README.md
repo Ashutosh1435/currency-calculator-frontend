c82eb3f946b6da610cd28cf81fab70ac4d9fd442
Ashutosh Singh <ashutosh.chauhan837@gmail.com>
2024-01-06 14:33:06 +0530

    important fixes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+### Follow the bellow steps to run the project locally
+
+1) Make sure you have install node and npm on you computer.
+2) Now, Run **npm install** on root directory for downloading node_modules.
+3) Now Run **npm run start** to start the local node server.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -14,10 +20,6 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
 ### `npm run build`
 
--- src/App.js
+++ src/App.js
@@ -1,63 +1,77 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
+import { SupportedCurrencies } from "./utils";
 
 function App() {
   const [topCurrencies, setTopCurrencies] = useState([]);
-  const [selectedCrypto, setSelectedCrypto] = useState('');
-  const [amount, setAmount] = useState('');
+  const [selectedCrypto, setSelectedCrypto] = useState("");
+  const [amount, setAmount] = useState("");
   const [supportedCurrencies, setSupportedCurrencies] = useState([]);
-  const [selectedCurrency, setSelectedCurrency] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState("");
+  const [result, setResult] = useState();
+  const [error, setError] = useState("");
   useEffect(() => {
     fetchTopCurrencies();
-    fetchSupportedCurrencies()
+    setSupportedCurrencies(SupportedCurrencies);
   }, []);
 
   const fetchTopCurrencies = async () => {
     try {
-      const response = await fetch('http://localhost:4000/api/topCurrencies');
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/topCurrencies`
+      );
       const data = await response.json();
       setTopCurrencies(data.data);
     } catch (error) {
-      console.error('Error fetching top currencies:', error);
+      console.error("Error fetching top currencies:", error);
     }
   };
 
   const handleSelectChange = (event) => {
     setSelectedCrypto(event.target.value);
-    // Perform any desired action when a cryptocurrency is selected
-    alert(`You selected: ${event.target.value}`);
+    if (error) setError("");
+    if (result) setResult(null);
   };
   const handleAmountChange = (event) => {
     setAmount(event.target.value);
+    if (error) setError("");
+    if (result) setResult(null);
   };
 
   const handleCurrencyChange = (event) => {
     setSelectedCurrency(event.target.value);
-  };
-  const fetchSupportedCurrencies = async () => {
-    try {
-      const response = await fetch('http://localhost:4000/api/supportedCurrencies');
-      const data = await response.json();
-      setSupportedCurrencies(data);
-    } catch (error) {
-      console.error('Error fetching supported currencies:', error);
-    }
+    if (error) setError("");
+    if (result) setResult(null);
   };
 
   const handleConvertCurrency = async () => {
-    const response = await fetch(`http://localhost:4000/api/convertCurrency?cryptoSymbol=${selectedCrypto}&targetCurrency=${selectedCurrency}&amount=${amount}`)
+    const response = await fetch(
+      `${process.env.REACT_APP_BASE_URL}/convertCurrency?cryptoSymbol=${selectedCrypto}&targetCurrency=${selectedCurrency}&amount=${amount}`
+    );
     const data = await response.json();
-    console.log(data)
+    if (data.convertedAmount) {
+      setResult(data);
+      setError("");
+      alert(`Converted Amount : ${data.convertedAmount}`);
+    } else
+      setError(`Free version of API doesn't support this crypto currency.`);
   };
 
-
   return (
-    <>
-      <div class="font-manrope flex h-screen w-full items-center justify-center">
+    <React.Fragment>
+      <div class="font-manrope flex mt-20  justify-center">
         <div class="mx-auto box-border w-[365px] border bg-white p-4">
-          <select value={selectedCrypto} onChange={handleSelectChange} class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-            <option value="" disabled>Select a cryptocurrency</option>
-            {topCurrencies?.map(crypto => (
+          <select
+            value={selectedCrypto}
+            onChange={handleSelectChange}
+            class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-state"
+          >
+            <option value="" disabled>
+              Select a cryptocurrency
+            </option>
+            {topCurrencies?.map((crypto) => (
               <option key={crypto.id} value={crypto.name}>
                 {crypto.name}
               </option>
@@ -66,19 +80,27 @@ function App() {
           <div class="mt-6">
             <div class="font-semibold">How much would you like to convert?</div>
             <div>
-              <input type="number"
+              <input
+                type="number"
                 placeholder="Enter amount"
                 value={amount}
                 onChange={handleAmountChange}
-                class="mt-1 w-full rounded-[4px] border border-[#A0ABBB] p-2" />
+                class="mt-1 w-full rounded-[4px] border border-[#A0ABBB] p-2"
+              />
             </div>
-
           </div>
 
           <div class="mt-6">
             <div class="font-semibold">From</div>
-            <select value={selectedCurrency} onChange={handleCurrencyChange} class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-              <option value="" disabled>Select a Currency</option>
+            <select
+              value={selectedCurrency}
+              onChange={handleCurrencyChange}
+              class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-state"
+            >
+              <option value="" disabled>
+                Select a Currency
+              </option>
               {supportedCurrencies?.map((currency, id) => (
                 <option key={crypto.id} value={currency}>
                   {currency}
@@ -88,11 +110,24 @@ function App() {
           </div>
 
           <div class="mt-6">
-            <button onClick={handleConvertCurrency} class="w-full cursor-pointer rounded-[4px] bg-green-700 px-3 py-[6px] text-center font-semibold text-white">Convert Currency</button>
+            <button
+              disabled={!amount || !selectedCrypto || !selectedCurrency}
+              onClick={handleConvertCurrency}
+              className="btn w-full cursor-pointer rounded-[4px] bg-green-700 px-3 py-[6px] text-center font-semibold text-white"
+            >
+              Convert Currency
+            </button>
           </div>
         </div>
       </div>
-    </>
+      <div className="mt-10 flex justify-center">
+        {error ? (
+          <p className="error">{error}</p>
+        ) : result ? (
+          <p>{"Converted Amount : " + result.convertedAmount}</p>
+        ) : null}
+      </div>
+    </React.Fragment>
   );
 }
 
